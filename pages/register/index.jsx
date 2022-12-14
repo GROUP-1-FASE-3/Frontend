fa2612b7271c91dc5793b22f6aa357e1c7a9f308
mitro-ubaidillah <ubaidillahmitro@gmail.com>
2022-12-14 10:47:29 +0700

    add validation yup for form register

--- pages/register/index.jsx
+++ pages/register/index.jsx
@@ -4,7 +4,7 @@ import AuthPage from '../../Components/AuthPage';
 import { IoEye, IoEyeOff } from 'react-icons/io5';
 import { FaUser } from 'react-icons/fa';
 import api from '../../services/api';
-import * as yup from 'yup';
+import * as Yup from 'yup';
 import Router from 'next/router';
 
 const Index = () => {
@@ -15,47 +15,64 @@ const Index = () => {
     const [phone_number, setPhone] = useState('');
     const [user_images, setUserImages] = useState('');
     const [hidePassword, setHidePassword] = useState(true);
+    const [errors, setErrors] = useState({});
 
-    let schema = yup.object().shape({
-        user_name: yup.string().required(),
-        email: yup.string().email().required(),
-        password: yup.string().required('Password is required').min(8)
+    let schema = Yup.object().shape({
+        user_name: Yup.string().required('username is required'),
+        email: Yup.string().email('has an email').required('Email is required'),
+        password: Yup.string().min(8, "Password has min 8 character").required('Password is required')
     });
 
     const onRegisterHandler = async () => {
-        await schema
-            .isValid({
-                user_name: user_name,
-                email: email,
-                password: password
+        await api.register({ user_name, email, password, gender, phone_number, user_images })
+            .then(response => {
+                console.log(response.data)
+                Router.push({
+                    pathname: `/login`
+                })
             })
-            .then((valid) => {
-                console.log(valid)
-                if (valid) {
-                    api.register({ user_name, email, password, gender, phone_number, user_images })
-                        .then(response => {
-                            console.log(response.data)
-                            Router.push({
-                                pathname: `/login`
-                            })
-                        })
-                        .catch(error => {
-                            alert(error)
-                        })
-                } else {
-                    alert(valid)
-                }
+            .catch(error => {
+                alert(error)
             })
 
     }
 
     const onSubmitRegisterHandler = (e) => {
-        onRegisterHandler();
         e.preventDefault();
-        setEmail('')
-        setPassword('')
-        setUsername('')
+        try {
+            schema.validateSync(
+                {
+                    user_name,
+                    email,
+                    password
+                },
+                {
+                    abortEarly: false
+                }
+            );
+        } catch (err) {
+            const { inner } = err;
+            let formErrors = {};
+
+            if (inner && inner[0]) {
+                inner.forEach(error => {
+                    const { path, message } = error;
+
+                    if (!formErrors[path]) {
+                        formErrors[path] = message;
+                    }
+                });
+            }
+            setErrors(formErrors);
+        }
+
+        // onRegisterHandler();
+        //     setEmail('')
+        // setPassword('')
+        // setUsername('')
     }
+
+    console.log(errors.user_name)
 
     return (
         <AuthPage>
@@ -70,12 +87,14 @@ const Index = () => {
                             placeholder="your username"
                             value={user_name}
                             onChange={(e) => setUsername(e.target.value)}
-                            className="p-5 bg-[#3333330F] w-full border-b border-[#00000061] placeholder:text-black rounded-tr rounded-tl focus:outline-none focus:rounded focus:ring-2 focus:ring-stay-primary"
+                            className={`p-5 bg-[#3333330F] w-full ${errors.user_name === undefined ? `border-[#00000061] border-b` : `border-error border-b-2`}  placeholder:text-black rounded-tr rounded-tl focus:outline-none focus:rounded focus:ring-2 focus:ring-stay-primary`}
                         />
                         <label htmlFor="" className='absolute text-2xl bottom-5 right-5 cursor-pointer text-[#00000061]' onClick={() => setHidePassword(!hidePassword)}>
                             <FaUser />
                         </label>
                     </div>
+                    <small className='text-error'>{errors.user_name}</small>
+
                 </div>
                 <div className="form-control w-full">
                     <label className="label">
@@ -87,12 +106,13 @@ const Index = () => {
                             placeholder="yourmail"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
-                            className="p-5  bg-[#3333330F] w-full border-b border-[#00000061] placeholder:text-black rounded-tr rounded-tl focus:outline-none focus:rounded focus:ring-2 focus:ring-stay-primary"
+                            className={`p-5 bg-[#3333330F] w-full ${errors.user_name === undefined ? `border-[#00000061] border-b` : `border-error border-b-2`}  placeholder:text-black rounded-tr rounded-tl focus:outline-none focus:rounded focus:ring-2 focus:ring-stay-primary`}
                         />
                         <label htmlFor="" className='absolute bottom-5 right-5 cursor-pointer text-[#00000061]' onClick={() => setHidePassword(!hidePassword)}>
                             @gmail.com
                         </label>
                     </div>
+                    <small className='text-error'>{errors.email}</small>
                 </div>
                 <div className="form-control w-full">
                     <label className="label">
@@ -103,7 +123,7 @@ const Index = () => {
                             placeholder="absdgr&#"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            className="p-5 w-full bg-[#3333330F] border-b border-[#00000061] placeholder:text-black rounded-tr rounded-tl focus:outline-none focus:rounded focus:ring-2 focus:ring-stay-primary"
+                            className={`p-5 bg-[#3333330F] w-full ${errors.user_name === undefined ? `border-[#00000061] border-b` : `border-error border-b-2`} placeholder:text-black rounded-tr rounded-tl focus:outline-none focus:rounded focus:ring-2 focus:ring-stay-primary`}
                         />
                         {
                             password !== '' && (
@@ -113,6 +133,7 @@ const Index = () => {
                             )
                         }
                     </div>
+                    <small className='text-error'>{errors.password}</small>
                 </div>
                 <div className="mt-6 w-full text-end">
                     <button type='submit' className="btn bg-stay-secondary w-52 border-none">Register</button>
