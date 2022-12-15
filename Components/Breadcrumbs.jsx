8331e4ce42ced8a6d7c113e0b6fcb2b7018dd224
mitro-ubaidillah <ubaidillahmitro@gmail.com>
2022-12-15 11:33:03 +0700

    fix error breadcrumb

--- Components/Breadcrumbs.jsx
+++ Components/Breadcrumbs.jsx
@@ -7,7 +7,7 @@ const Breadcrumbs = ({ page, href }) => {
             <div className="text-[18px] flex gap-3 font-bold text-[#B0B0B0]">
                 <Link href='/'>Home</Link>
                 <p>/</p>
-                <p className='text-stay-primary'>{page}</p>
+                <p className='text-primary'>{page}</p>
             </div>
         </div>
     );
